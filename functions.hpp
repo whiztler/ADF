b556cb3ed0d958359efc7e319853bda40a2c27b9
whiztler <wolfpack.missions@gmail.com>
2015-05-20 08:28:32 -0500

    Moved config functions from description.ext to config.hpp

--- functions.hpp
+++ functions.hpp
@@ -12,4 +12,37 @@ File: functions.hpp
 Functions registration goes in here.
 ****************************************************************/
 
+class TAWVD
+{
+	tag = "TAWVD";
+	class TAW_VD
+	{
+		file = "Core\TP\taw_vd";
+		class onSliderChange {};
+		class onTerrainChange {};
+		class updateViewDistance {};
+		class openTAWVD {};
+		class trackViewDistance {};
+		class tawvdInit {postInit = 1;};
+	};
+};
 
+class fspectator
+{
+	file = "Core\TP\spect";
+	class CamInit{};
+	class OnUnload{};
+	class DrawTags{};
+	class EventHandler{};
+	class FreeCam{};
+	class GetPlayers{};
+	class ReloadModes{};
+	class UpdateValues{};
+	class HandleCamera{};
+	class ToggleGUI{};
+	class OnMapClick{};
+	class DrawMarkers{};
+	class ForceExit{};
+	class HandleMenu{};
+	class showMenu{};
+};
