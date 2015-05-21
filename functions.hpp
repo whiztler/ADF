f46c03ce138a489f13a0928b892782f0fc35f336
whiztler <wolfpack.missions@gmail.com>
2015-05-20 19:03:41 -0500

    updated for F3 spect

--- functions.hpp
+++ functions.hpp
@@ -11,38 +11,42 @@ File: functions.hpp
 ****************************************************************
 Functions registration goes in here.
 ****************************************************************/
-
-class TAWVD
+class CfgFunctions
 {
-	tag = "TAWVD";
-	class TAW_VD
+	class TAWVD
 	{
-		file = "Core\TP\taw_vd";
-		class onSliderChange {};
-		class onTerrainChange {};
-		class updateViewDistance {};
-		class openTAWVD {};
-		class trackViewDistance {};
-		class tawvdInit {postInit = 1;};
+		tag = "TAWVD";
+		class TAW_VD {
+			file = "Core\TP\taw_vd";
+			class onSliderChange {};
+			class onTerrainChange {};
+			class updateViewDistance {};
+			class openTAWVD {};
+			class trackViewDistance {};
+			class tawvdInit {postInit = 1;};
+		};
 	};
-};
 
-class fspectator
-{
-	file = "Core\TP\spect";
-	class CamInit{};
-	class OnUnload{};
-	class DrawTags{};
-	class EventHandler{};
-	class FreeCam{};
-	class GetPlayers{};
-	class ReloadModes{};
-	class UpdateValues{};
-	class HandleCamera{};
-	class ToggleGUI{};
-	class OnMapClick{};
-	class DrawMarkers{};
-	class ForceExit{};
-	class HandleMenu{};
-	class showMenu{};
+	class F // F3 Framework (http://ferstaberinde.com/f3/en/)
+	{	
+		class fspectator
+		{
+			file = "Core\TP\spect";
+			class CamInit{};
+			class OnUnload{};
+			class DrawTags{};
+			class EventHandler{};
+			class FreeCam{};
+			class GetPlayers{};
+			class ReloadModes{};
+			class UpdateValues{};
+			class HandleCamera{};
+			class ToggleGUI{};
+			class OnMapClick{};
+			class DrawMarkers{};
+			class ForceExit{};
+			class HandleMenu{};
+			class showMenu{};
+		};
+	};
 };
