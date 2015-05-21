f7d0aac0a920f633c4c3ec82737a25280e21d78d
whiztler <wolfpack.missions@gmail.com>
2015-05-20 19:04:25 -0500

    was cfg.hpp

---
+++ missionConfig.hpp
@@ -0,0 +1,32 @@
+/****************************************************************
+ARMA Mission Development Framework
+ADF version: 1.39 / MAY 2015
+
+Script: Cfg entries
+Author: Whiztler
+Script version: 1.0
+
+Game type: n/a
+File: missionConfig.hpp
+****************************************************************
+Config entry registration goes in here.
+****************************************************************/
+
+class CfgUnitInsignia {
+	class CLANPATCH {
+		displayName = "Nopryl"; // Name displayed in Arsenal
+		author = "ADF / Whiztler";
+		texture = "Img\clan_patch_Nopryl.paa";
+		textureVehicle = ""; // Does nothing currently, reserved for future use
+	};
+};
+
+class CfgRespawnTemplates { // F3 Spectator Script
+	class f_Spectator {
+		onPlayerRespawn = "f_fnc_CamInit";
+	};
+
+    class Seagull { //Overwrite Vanilla Seagull
+        onPlayerRespawn = "";
+    };
+};
